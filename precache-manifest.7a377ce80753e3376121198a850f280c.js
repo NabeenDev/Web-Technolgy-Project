118ab174da0f19cd0b9b7fe05d07e01d214ada63
Nabeen <nabeendev577@gmail.com>
2025-06-03 02:15:35 +0500

    build

---
+++ precache-manifest.7a377ce80753e3376121198a850f280c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed3609883275480362465aed6ab191de",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c4244ae6ff181c878a9d",
+    "url": "/static/css/main.708a4d9b.chunk.css"
+  },
+  {
+    "revision": "bd6bd55d0f17c3088198",
+    "url": "/static/js/2.b190f742.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.b190f742.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c4244ae6ff181c878a9d",
+    "url": "/static/js/main.ceae5811.chunk.js"
+  },
+  {
+    "revision": "fd1e764c12408bbbbfc1",
+    "url": "/static/js/runtime-main.f859b388.js"
+  }
+]);
